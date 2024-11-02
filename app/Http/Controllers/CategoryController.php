b3dbc05cf5e64ec04250dbafd44b716dfd5993f5
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2024-11-02 09:06:18 +0700

    Menampilkan Data Categories

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -15,7 +15,10 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
+        $categories = Category::all();
+        return view('admin.categories.index', [
+            'categories' => $categories
+        ]);
     }
 
     /**
